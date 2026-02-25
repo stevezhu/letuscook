c938b2ccb9677289afbdf23e98cd891df87cf825
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2026-02-24 23:33:58 -0500

    Format files

--- apps/assistant-desktop/src/renderer/src/main.tsx
+++ apps/assistant-desktop/src/renderer/src/main.tsx
@@ -1,7 +1,11 @@
 import './main.css';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from '@tanstack/react-router';
-import { ApiClientProvider, queryClient, router } from '@workspace/assistant-app';
+import {
+  ApiClientProvider,
+  queryClient,
+  router,
+} from '@workspace/assistant-app';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
