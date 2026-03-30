2969c113772e3a51a7b58d68b30c5aee3267d0d8
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2026-03-30 18:22:31 -0400

    Fix mock

---
+++ apps/assistant-mobile/src/modules/auth/react/__mocks__/auth-provider.tsx
@@ -0,0 +1,37 @@
+import { type useAuth as useAuthType } from '../auth-provider.tsx';
+
+type UseAuthParams = Parameters<typeof useAuthType>;
+type UseAuthReturn = ReturnType<typeof useAuthType>;
+
+export const useAuth = jest
+  .fn<UseAuthReturn, UseAuthParams>()
+  .mockImplementation(() => {
+    return {
+      user: {
+        id: 'test',
+        email: 'test@test.com',
+        firstName: 'Test',
+        lastName: 'User',
+        profilePictureUrl: 'https://test.com/profile.png',
+      },
+      isLoading: false,
+      getAccessToken: jest
+        .fn<
+          ReturnType<UseAuthReturn['getAccessToken']>,
+          Parameters<UseAuthReturn['getAccessToken']>
+        >()
+        .mockImplementation(async () => 'test-token'),
+      signIn: jest
+        .fn<
+          ReturnType<UseAuthReturn['signIn']>,
+          Parameters<UseAuthReturn['signIn']>
+        >()
+        .mockImplementation(async () => ({ success: true })),
+      signOut: jest
+        .fn<
+          ReturnType<UseAuthReturn['signOut']>,
+          Parameters<UseAuthReturn['signOut']>
+        >()
+        .mockImplementation(async () => ({ success: true })),
+    };
+  });
