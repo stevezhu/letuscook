192178f3a09ec51a914d5f5aceee948d484b3620
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2026-03-30 11:57:25 -0400

    Adjust oxc configs

---
+++ oxfmt.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, type OxfmtConfig } from 'oxfmt';
+
+export default defineConfig<OxfmtConfig>({
+  sortImports: {},
+  sortPackageJson: {
+    sortScripts: true,
+  },
+  ignorePatterns: [
+    '.vite/',
+    'migrations/',
+    '_generated/',
+    '*.gen.ts',
+    '*.gen.d.ts',
+    'uniwind-types.d.ts',
+    'storybook.requires.ts',
+  ],
+  overrides: [
+    {
+      files: ['*.jsonc'],
+      options: {
+        // XXX: do not use trailing commas for jsonc files
+        // Reference: https://github.com/prettier/prettier/issues/15956#issuecomment-3000347490
+        trailingComma: 'none',
+      },
+    },
+  ],
+  printWidth: 80,
+  singleQuote: true,
+});
